961c8ea099099a4cce4db2ea5cf9e7e93b10e83a
Manan Shah <manan@Manans-MacBook-Pro.local>
2025-06-25 23:15:55 -0700

    setup knex config and delete terraform files

---
+++ db/migrations/20250626060116_create_daily_fixtures_routed_table.js
@@ -0,0 +1,46 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('daily_fixtures_routed', function (table) {
+    table
+      .string('id')
+      .primary()
+      .defaultTo(
+        knex.raw(
+          "concat('dfr', lpad(floor(random() * 1000000)::text, 6, '0'))"
+        )
+      );
+    table.string('fixture_id').notNullable();
+    table.string('match_id').notNullable(); 
+    table.timestampz('date_time_of_match').notNullable();
+    table.string('fixture_type').notNullable(); 
+    
+
+    // Users
+    table.integer("count_users_to_inform").notNullable();
+    table.integer("count_users_successfully_notified").notNullable();
+    table.integer("count_users_failed_to_notify").notNullable();
+
+    // Notification
+    table.string('notification_type').notNullable().checkIn(['email', 'slack']).defaultTo('slack');
+    table.string('notification_status').notNullable().checkIn(['partial', 'success', 'failed','pending']).defaultTo('pending');
+    table.timestampz('date_time_of_notification_sent').nullable();
+
+
+    // Timestamps
+    table.timestamp('date_created').defaultTo(knex.fn.now());
+    table.timestamp('date_updated').defaultTo(knex.fn.now());
+    table.timestamp('date_deleted').nullable();
+
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('daily_fixtures_routed');
+};
--- terraform/modules/security_group/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-variable "cidr_block_ingress" {
-  description =  "CIDR block for ingress"
-  type = list(string)
-} 
