462d5fe4c003052255d3f9b6be492b565fa25bf6
Manan <157731770+mananshah78424@users.noreply.github.com>
2025-06-25 23:17:04 -0700

    setup knex config and delete terraform files (#4)
    Co-authored-by: Manan Shah <manan@Manans-MacBook-Pro.local>

---
+++ knexfile.js
@@ -0,0 +1,65 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// export default {
+//   development: {
+//     client: 'postgresql',
+//     connection: {
+//       host: process.env.DB_HOST,
+//       database: process.env.DB_NAME,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       port: process.env.DB_PORT,
+//       ssl: { rejectUnauthorized: false }
+//     },
+//     migrations: {
+//       directory: './db/migrations'
+//     }
+//   }
+// };
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+const baseConfig = {
+  client: 'postgresql',
+  migrations: {
+    directory: './db/migrations',
+  },
+  seeds: {
+    directory: './db/seeds',
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+};
+
+const knexConfig = {
+  development: {
+    ...baseConfig,
+    connection: {
+      host: "localhost",
+      database: "mike-docker",
+      user: "admin",
+      password: "admin",
+      port: 5431
+    },
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+  },
+};
+
+export default knexConfig;
--- terraform/modules/sqs/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_sqs_queue" "this" {
-    name = var.queue_name
-    message_retention_seconds = var.message_retention_seconds
-}
